890a12d4504b121fdbc5bcbd9497b2f4f4359a87
Yuta iwasaki <yanyong0127@gmail.com>
2021-09-23 15:52:25 +0900

    Initial Commit

---
+++ sidemenuintro2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sidemenuintro2
+//
+//  Created by IwasakIYuta on 2021/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
